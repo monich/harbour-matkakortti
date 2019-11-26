c087cb5ab1f77a98efa4f81bc088cff008839fa2
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-11-26 12:38:02 +0800

    Create harbour-matkakortti-zh_CN.ts

---
+++ translations/harbour-matkakortti-zh_CN.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="matkakortti-app_name">
+        <source>Matkakortti</source>
+        <extracomment>Application title</extracomment>
+        <translation>Matkakortti</translation>
+    </message>
+    <message id="matkakortti-info-nfc_not_supported">
+        <source>NFC not supported</source>
+        <extracomment>Info label</extracomment>
+        <translation>不支持NFC</translation>
+    </message>
+    <message id="matkakortti-info-card_not_supported">
+        <source>This is not an HSL travel card</source>
+        <extracomment>Info label</extracomment>
+        <translation>该卡非 HSL 旅行卡</translation>
+    </message>
+    <message id="matkakortti-info-reading">
+        <source>Reading the card</source>
+        <extracomment>Info label</extracomment>
+        <translation>读取卡片</translation>
+    </message>
+    <message id="matkakortti-info-ready">
+        <source>Ready</source>
+        <extracomment>Info label</extracomment>
+        <translation>就绪</translation>
+    </message>
+    <message id="matkakortti-info-disabled">
+        <source>NFC is disabled</source>
+        <extracomment>Info label</extracomment>
+        <translation>已禁用 NFC</translation>
+    </message>
+    <message id="matkakortti-info-touch_hint">
+        <source>Place the phone on the card</source>
+        <extracomment>Hint label</extracomment>
+        <translation>将手机放置于卡片之上</translation>
+    </message>
+    <message id="matkakortti-card-header">
+        <source>Travel card</source>
+        <extracomment>Page title</extracomment>
+        <translation>旅行卡</translation>
+    </message>
+    <message id="matkakortti-time_unit_abbr-min">
+        <source>min</source>
+        <extracomment>Time unit (abbreviated)</extracomment>
+        <translation>分钟</translation>
+    </message>
+    <message id="matkakortti-time_unit_abbr-hour">
+        <source>h</source>
+        <extracomment>Time unit (abbreviated)</extracomment>
+        <translation>小时</translation>
+    </message>
+    <message id="matkakortti-time_unit_abbr-24hour">
+        <source>d</source>
+        <extracomment>Time unit (abbreviated)</extracomment>
+        <translation>天</translation>
+    </message>
+    <message id="matkakortti-time_unit_abbr-day">
+        <source>d</source>
+        <extracomment>Time unit (abbreviated)</extracomment>
+        <translation>天</translation>
+    </message>
+    <message id="matkakortti-switcher-details">
+        <source>Details</source>
+        <extracomment>Switcher label</extracomment>
+        <translation>详情</translation>
+    </message>
+    <message id="matkakortti-switcher-history">
+        <source>History</source>
+        <extracomment>Switcher label</extracomment>
+        <translation>历史</translation>
+    </message>
+    <message id="matkakortti-details-section-season_tickets">
+        <source>Season tickets</source>
+        <extracomment>Section header</extracomment>
+        <translation>季卡</translation>
+    </message>
+    <message id="matkakortti-details-zone">
+        <source>Zone:</source>
+        <extracomment>Label</extracomment>
+        <translation>区域：</translation>
+    </message>
+    <message id="matkakortti-details-section-card_value">
+        <source>Card value</source>
+        <extracomment>Section header</extracomment>
+        <translation>卡片金额</translation>
+    </message>
+    <message id="matkakortti-details-loaded_value-label">
+        <source>Last loaded value:</source>
+        <extracomment>Label</extracomment>
+        <translation>最近读取金额：</translation>
+    </message>
+    <message id="matkakortti-details-loading_time-label">
+        <source>Loading time:</source>
+        <extracomment>Label</extracomment>
+        <translation>读卡时间：</translation>
+    </message>
+    <message id="matkakortti-details-section-latest_ticket">
+        <source>Latest value ticket</source>
+        <extracomment>Section header</extracomment>
+        <translation>最近储值票</translation>
+    </message>
+    <message id="matkakortti-details-ticket-group_size">
+        <source>Group size:</source>
+        <extracomment>Label</extracomment>
+        <translation>卡组大小：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-cost">
+        <source>Cost:</source>
+        <extracomment>Label</extracomment>
+        <translation>花费：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-validity_length">
+        <source>Validity time:</source>
+        <extracomment>Label</extracomment>
+        <translation>有效期：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-valid_from">
+        <source>Valid from:</source>
+        <extracomment>Label</extracomment>
+        <translation>有效期始于：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-boarding_time">
+        <source>Boarding time:</source>
+        <extracomment>Label</extracomment>
+        <translation>上车时间：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-valid_until">
+        <source>Valid until:</source>
+        <extracomment>Label</extracomment>
+        <translation>有效期截止于：</translation>
+    </message>
+    <message id="matkakortti-details-ticket-valid_until-suffix">
+        <source> </source>
+        <extracomment>Suffix after the time ending the period</extracomment>
+        <translation> </translation>
+    </message>
+    <message id="matkakortti-card-validity-valid">
+        <source>Valid</source>
+        <extracomment>Validity label</extracomment>
+        <translation>有效</translation>
+    </message>
+    <message id="matkakortti-card-validity-not_yet_valid">
+        <source>Not yet valid</source>
+        <extracomment>Validity label</extracomment>
+        <translation>尚未生效</translation>
+    </message>
+    <message id="matkakortti-card-validity-no_longer_valid">
+        <source>No longer valid</source>
+        <extracomment>Validity label</extracomment>
+        <translation>不再有效</translation>
+    </message>
+    <message id="matkakortti-history-section-previous_journeys">
+        <source>Previous journeys</source>
+        <extracomment>Section header</extracomment>
+        <translation>旅行记录</translation>
+    </message>
+    <message id="matkakortti-history-transaction_type">
+        <source>Season ticket or boarding</source>
+        <extracomment>Label (transaction type)</extracomment>
+        <translation>季卡或单次登车</translation>
+    </message>
+</context>
+</TS>
